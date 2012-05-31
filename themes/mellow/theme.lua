b7782ca9be969cccab4bd74d94033c879f216a92
Lauri Laaksonen <lhlaaksonen@gmail.com>
2012-05-31 22:32:12 +0100

    Initial commit.

---
+++ themes/mellow/theme.lua
@@ -0,0 +1,116 @@
+----------------------------
+---------- Mellow ----------
+----------------------------
+
+
+theme = {}
+
+-- {{{ Main
+
+theme.font          = "Dina 8"
+
+theme.wallpaper_cmd = { "awsetbg /home/lauri/data/wallpapers/cat2.jpg" }
+
+-- }}}
+
+
+-- {{{ Colors
+
+theme.bg_normal     = "#070707"
+theme.bg_focus      = "#070707"
+theme.bg_urgent     = "#070707"
+theme.bg_minimize   = "#444444"
+
+theme.fg_normal     = "#ecedee"
+theme.fg_focus      = "#6b8ba3"
+theme.fg_urgent     = "#927d9e"
+theme.fg_minimize   = "#ffffff"
+
+theme.border_width  = "1"
+theme.border_normal = "#1c1c1c"
+theme.border_focus  = "#265167"
+theme.border_marked = "#3ca4d8"
+
+theme.mouse_finder_color = "#6b8ba3"
+
+-- }}}
+
+
+-- {{{ Menu
+
+theme.menu_height = "8"
+theme.menu_width  = "105"
+theme.menu_border_width  = "0"
+theme.menu_fg_normal = "#ecedee"
+theme.menu_fg_focus =  "#6b8ba3"
+theme.menu_bg_normal = "#000000dd"
+theme.menu_bg_focus  = "#000000dd"
+
+-- }}}
+
+
+-- {{{ Taglist 
+
+theme.taglist_squares_sel   = "/home/lauri/.config/awesome/themes/mellow/taglist/squaref_b.png" 
+theme.taglist_squares_unsel = "/home/lauri/.config/awesome/themes/mellow/taglist/square_b.png" 
+
+-- }}}
+
+
+-- {{{ Titlebar 
+
+theme.titlebar_close_button_focus  = "/home/lauri/.config/awesome/themes/mellow/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = "/home/lauri/.config/awesome/themes/mellow/titlebar/close_normal.png"
+
+theme.titlebar_ontop_button_focus_active  = "/home/lauri/.config/awesome/themes/mellow/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active = "/home/lauri/.config/awesome/themes/mellow/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive  = "/home/lauri/.config/awesome/themes/mellow/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = "/home/lauri/.config/awesome/themes/mellow/titlebar/ontop_normal_inactive.png"
+
+theme.titlebar_sticky_button_focus_active  = "/home/lauri/.config/awesome/themes/mellow/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active = "/home/lauri/.config/awesome/themes/mellow/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive  = "/home/lauri/.config/awesome/themes/mellow/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = "/home/lauri/.config/awesome/themes/mellow/titlebar/sticky_normal_inactive.png"
+
+theme.titlebar_floating_button_focus_active  = "/home/lauri/.config/awesome/themes/mellow/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active = "/home/lauri/.config/awesome/themes/mellow/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive  = "/home/lauri/.config/awesome/themes/mellow/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = "/home/lauri/.config/awesome/themes/mellow/titlebar/floating_normal_inactive.png"
+
+theme.titlebar_maximized_button_focus_active  = "/home/lauri/.config/awesome/themes/mellow/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active = "/home/lauri/.config/awesome/themes/mellow/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = "/home/lauri/.config/awesome/themes/mellow/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = "/home/lauri/.config/awesome/themes/mellow/titlebar/maximized_normal_inactive.png"
+
+-- }}}
+
+
+-- {{{ Layout
+
+theme.layout_tile       = "/home/lauri/.config/awesome/themes/mellow/layouts/tile.png"
+theme.layout_tileleft   = "/home/lauri/.config/awesome/themes/mellow/layouts/tileleft.png"
+theme.layout_tilebottom = "/home/lauri/.config/awesome/themes/mellow/layouts/tilebottom.png"
+theme.layout_tiletop    = "/home/lauri/.config/awesome/themes/mellow/layouts/tiletop.png"
+theme.layout_fairv      = "/home/lauri/.config/awesome/themes/mellow/layouts/fairv.png"
+theme.layout_fairh      = "/home/lauri/.config/awesome/themes/mellow/layouts/fairh.png"
+theme.layout_spiral     = "/home/lauri/.config/awesome/themes/mellow/layouts/spiral.png"
+theme.layout_dwindle    = "/home/lauri/.config/awesome/themes/mellow/layouts/dwindle.png"
+theme.layout_max        = "/home/lauri/.config/awesome/themes/mellow/layouts/max.png"
+theme.layout_fullscreen = "/home/lauri/.config/awesome/themes/mellow/layouts/fullscreen.png"
+theme.layout_magnifier  = "/home/lauri/.config/awesome/themes/mellow/layouts/magnifier.png"
+theme.layout_floating   = "/home/lauri/.config/awesome/themes/mellow/layouts/floating.png"
+
+-- }}}
+
+
+-- {{{ Misc
+
+theme.awesome_icon = "/home/lauri/.config/awesome/themes/mellow/widgets/arch_10x10.png"
+theme.menu_submenu_icon = "/home/lauri/.config/awesome/themes/mellow/submenu.png"
+theme.tasklist_floating_icon = "/home/lauri/.config/awesome/themes/mellow/floating.png"
+
+-- }}}
+
+
+return theme
+
